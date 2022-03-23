bf1e0c47544e9de37de62893dc585476356cb046
Grace Barbara <98533301+gracebarbara@users.noreply.github.com>
2022-03-23 02:33:41 +0000

    part a

--- main.py
+++ main.py
@@ -31,30 +31,76 @@ import time
 #########################################################
 #                   Your Code Goes Below                #
 #########################################################
-def drawsquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
-  side=2
+def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
+  myturtle.penup()
+  myturtle.goto(-1, -1)
+  myturtle.pendown()
   for i in range(4):
-    darty.forward(side)
-    darty.left(90)
+    myturtle.forward(width)
+    myturtle.left(90)
+  myturtle.penup()
 
 def drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0):
-  darty.backward(1)
-  darty.forward(2)
+  myturtle.goto(x_start, 0)
+  myturtle.pendown()
+  myturtle.goto(x_end, 0)
+  myturtle.penup()
+  myturtle.goto(0, y_start)
+  myturtle.pendown()
+  myturtle.goto(0, y_end)
+  myturtle.penup()
 
 def drawCircle(myturtle=None, radius=0):
-  radius = 1
-  dartyl.circle(radius)
+  myturtle.goto(0, -1)
+  myturtle.pendown()
+  myturtle.circle(radius)
+  myturtle.penup()
 
 def setUpDartboard(myscreen=None, myturtle=None):
+  myscreen.setworldcoordinates(-1.5, -1.5, 1.5, 1.5)
+  drawSquare(myturtle, 2, -1, 1)
+  drawLine(myturtle, -1, 0, 1, 0)
+  drawLine(myturtle, 0, -1, 0, 1)
+  drawCircle(myturtle, 1)
+
+#def isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0):
+  #randx = random.uniform(-1, 1)
+  #randy = random.uniform(-1, 1)
+  #circ_eq = (randx - circle_center_x)**2 + (randy - circle_center_y)**2
+  #rad_eq = radius**2
+  #if circ_eq < rad_eq:
+    #myturtle.goto(randx, randy)
+    #myturtle.pendown()
+    #myturtle.dot("green")
+    #myturtle.penup()
+  #else:
+    #myturtle.goto(randx, randy)
+    #myturtle.pendown()
+    #myturtle.dot("red")
+    #myturtle.penup()
+
+
+def throwDart(myturtle=0):
+  randx = random.uniform(-1, 1)
+  randy = random.uniform(-1, 1)
+  distance = myturtle.distance(randx, randy)
+  if distance > 1:
+    myturtle.goto(randx, randy)
+    myturtle.pendown()
+    myturtle.dot("red")
+    myturtle.penup()
+  else:
+    myturtle.goto(randx, randy)
+    myturtle.pendown()
+    myturtle.dot("green")
+    myturtle.penup()
+
+def playDarts(myturtle=0):
+  
+ 
   
 
-def isInCircle(myturtle=None, circle_center_x=0, circle_center_y=0, radius=0):
-
-def throwDart(myturtle=None):
-
-def playDarts(myturtle=None):
-
-def montePi(myturtle=None, num_darts=0):
+  
   
     
 
@@ -63,7 +109,7 @@ def montePi(myturtle=None, num_darts=0):
 #         Do not alter any code below here              #
 #       Your code must work with the main proivided     #
 #########################################################
-def main():
+  def main():
     # Get number of darts for simulation from user
     # Note continuation character <\> so we don't go over 78 columns:
     print("This is a program that simulates throwing darts at a dartboard\n" \
@@ -105,4 +151,4 @@ def main():
     # Don't hide or mess with window while it's 'working'
     window.exitonclick()
 
-main()
+main() 
